d53e685e3de62485c097051722c6abb3ae3fd72e
Kamran Manzoor <mr.kami2899@gmail.com>
2019-06-19 17:02:41 +0500

    Added Akmal's code

---
+++ app/src/main/java/com/kamores/tiffin/Fragment/FragmentDay.java
@@ -0,0 +1,24 @@
+package com.kamores.tiffin.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kamores.tiffin.R;
+
+public class FragmentDay extends Fragment {
+    View view;
+    public FragmentDay() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate( R.layout.day_fragment,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/kamores/tiffin/Fragment/FragmentWeek.java
@@ -0,0 +1,25 @@
+package com.kamores.tiffin.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kamores.tiffin.R;
+
+public class FragmentWeek extends Fragment {
+    View view;
+
+    public FragmentWeek() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate( R.layout.week_fragment,container,false);
+        return view;
+    }
+}
