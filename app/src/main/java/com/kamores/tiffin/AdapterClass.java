4eb556b738ae83f4be795a7c5dce505a794975ae
Kamran Manzoor <mr.kami2899@gmail.com>
2019-06-21 17:55:25 +0500

    Push for Week class

--- app/src/main/java/com/kamores/tiffin/AdapterClass.java
+++ app/src/main/java/com/kamores/tiffin/AdapterClass.java
@@ -10,7 +10,6 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
