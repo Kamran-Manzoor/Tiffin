e13c0a8af36ce048826e0235c28823e1847ee576
Kamran <mr.kami2899@gmail.com>
2019-08-29 23:09:06 +0500

    Merge branch 'master' of https://github.com/kami2899/Tiffin
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.properties

--- app/src/main/java/com/kamores/tiffin/constants/Constants.java
+++ app/src/main/java/com/kamores/tiffin/constants/Constants.java
@@ -2,7 +2,7 @@ package com.kamores.tiffin.constants;
 
 public class Constants {
     //public static final String RETRIEVE_ALL = "allCardDataItem";
-    public static final String BASE_URL = "http://192.168.10.6:8888/";
+    public static final String BASE_URL = "http://192.168.10.4:8888/";
     public static final String REGISTER_SERVICE ="addServices";
     public static final String RETRIVE_DETAIL ="detailSup";
     public static final String RETRIVE_ITEMS ="allitemtoSupDay";
@@ -12,13 +12,9 @@ public class Constants {
     public static final String EMPTY ="empty";
     public static final String FAILURE ="failure";
     public static final String IS_LOGED_IN ="isLoggedIn";
-
     public static final String NAME ="name";
     public static final String EMAIL ="email";
     public static final String UNIQUE_ID ="unique_id";
-
-    public static final String TAG ="Learn2Crack";
-
     public static final String REGISTER_USER ="registerUser";
     public static final String LOG_IN ="login";
     public static final String REGISTER_SUPPLIER ="register_Supplier";
@@ -26,6 +22,9 @@ public class Constants {
     public static final String TODAY_MENU_ITEMS ="today_menu_items";
     public static final String PROFILE_MENU ="retrieveProfile";
     public static final String ADD_ITEMS ="addItems";
+
+    public static final String TAG = "TIFFIN APP";
+
 
 
 
